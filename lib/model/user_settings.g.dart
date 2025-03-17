ad4758eda81cc265eb1742951fe7a0214863bc3f
Luke Offerman <lukeofferman@yahoo.com>
2025-03-17 14:25:39 -0400

    v1

---
+++ lib/model/user_settings.g.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_settings.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class UserSettingsAdapter extends TypeAdapter<UserSettings> {
+  @override
+  final int typeId = 1;
+
+  @override
+  UserSettings read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return UserSettings(
+      rounding: fields[0] as String,
+      precision: fields[1] as num,
+      trainingMaxPercent: fields[3] as num,
+      createdAt: fields[4] as DateTime,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, UserSettings obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.rounding)
+      ..writeByte(1)
+      ..write(obj.precision)
+      ..writeByte(3)
+      ..write(obj.trainingMaxPercent)
+      ..writeByte(4)
+      ..write(obj.createdAt);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is UserSettingsAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
